b3d0ea3235c486bcc573a36835d5221b7e8cd17b
Joshua <87705198+joshuanoeldeke@users.noreply.github.com>
2024-01-30 22:36:52 +0100

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -16,6 +16,7 @@ if ($conn->connect_error) {
 $sql = "SELECT * FROM cart ORDER BY cart_id DESC LIMIT 1";
 $result = $conn->query($sql);
 
+$product_id = "";
 $product_name = "";
 $product_price = "";
 $product_quantity = "";
@@ -23,12 +24,14 @@ $product_quantity = "";
 if ($result->num_rows > 0) {
     // Daten für das neueste Produkt in der Tabelle 'cart' ausgeben
     $row = $result->fetch_assoc();
+    $product_id = $row["id"];
     $product_name = $row["name"];
     $product_price = $row["price"];
     $product_quantity = $row["quantity"];
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
     $firstname = $_POST["firstname"];
     $lastname = $_POST["lastname"];
     $street = $_POST["street"];
@@ -36,11 +39,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $postalcode = $_POST["postalcode"];
 
     // SQL-Abfrage, um die Daten in die Tabelle 'adress' einzufügen
-    $sql = "INSERT INTO adress (firstname, lastname, street, city, postalcode)
-    VALUES ('$firstname', '$lastname', '$street', '$city', '$postalcode')";
+    $sql = "INSERT INTO adress (email, firstname, lastname, street, city, postalcode)
+    VALUES ('$email', '$firstname', '$lastname', '$street', '$city', '$postalcode')";
 
     if ($conn->query($sql) === TRUE) {
-        echo "Neuer Eintrag erfolgreich erstellt";
+        $adress_id = $conn->insert_id;
+        $date = date('Y-m-d H:i:s');
+
+        // SQL-Abfrage, um die Daten in die Tabelle 'order_table' einzufügen
+        $sql = "INSERT INTO order_table (adress_id, date)
+        VALUES ('$adress_id', '$date')";
+
+        if ($conn->query($sql) === TRUE) {
+            $order_id = $conn->insert_id;
+
+            // SQL-Abfrage, um die Daten in die Tabelle 'order_products' einzufügen
+            $sql = "INSERT INTO order_products (order_id, product_id, amount)
+            VALUES ('$order_id', '$product_id', '$product_quantity')";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "Neuer Eintrag erfolgreich erstellt";
+            } else {
+                echo "Fehler: " . $sql . "<br>" . $conn->error;
+            }
+        } else {
+            echo "Fehler: " . $sql . "<br>" . $conn->error;
+        }
     } else {
         echo "Fehler: " . $sql . "<br>" . $conn->error;
     }
@@ -91,6 +115,10 @@ $conn->close();
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
     <h2>Ihre Daten :)</h2>
     <ul>
+        <li>
+            <label for="email">E-Mail:</label>
+            <input type="email" id="email" name="email" required>
+        </li>
         <li>
             <label for="firstname">Vorname:</label>
             <input type="text" id="firstname" name="firstname" required>
